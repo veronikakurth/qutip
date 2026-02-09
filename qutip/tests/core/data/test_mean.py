ee91eed93b736ed40a7425592931441b70adacc3
Your Name <veronika.rovnik@posteo.de>
2026-02-09 13:40:02 +0900

    Add tests for mean_abs functions

--- qutip/tests/core/data/test_mean.py
+++ qutip/tests/core/data/test_mean.py
@@ -4,6 +4,7 @@ import qutip as qt
 import numbers
 
 from qutip.core.data.mean import mean_csr, mean_dia, mean_dense
+from qutip.core.data.mean import mean_abs_csr, mean_abs_dia, mean_abs_dense
 from qutip.core.data import CSR, Dia, Dense
 from . import test_mathematics as testing
 
@@ -22,4 +23,21 @@ class TestMean(testing.UnaryOpMixin):
         pytest.param(mean_csr, CSR, numbers.Complex),
         pytest.param(mean_dia, Dia, numbers.Complex),
         pytest.param(mean_dense, Dense, numbers.Complex),
+    ]
+
+class TestAbsMean(testing.UnaryOpMixin):
+    def op_numpy(self, matrix):
+        nnz = np.count_nonzero(matrix)
+
+        if nnz == 0:
+            return 0.0
+
+        abs_matrix = np.abs(matrix)
+        mean_val = abs_matrix.sum() / nnz
+        return mean_val
+
+    specialisations = [
+        pytest.param(mean_abs_csr, CSR, numbers.Complex),
+        pytest.param(mean_abs_dia, Dia, numbers.Complex),
+        pytest.param(mean_abs_dense, Dense, numbers.Complex),
     ]
