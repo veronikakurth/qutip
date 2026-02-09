Fix accuracy error by adjusting expected value computation (numpy doesn't handle mean computation on sparse objects correctly; adjust test cases to qutip's style

qutip/tests/core/data/test_mean.py
@@ -7,114 +7,19 @@ from qutip.core.data.mean import mean_csr, mean_dia, mean_dense
 from qutip.core.data import CSR, Dia, Dense
 from . import test_mathematics as testing
 
-
-# Fixtures to be reused
-# TODO: this can belong the parametrization block and sub/super diagonal offsets can be computed based on dim
-@pytest.fixture
-def dim():
-  return 5
-
-@pytest.fixture
-def main_diag_offset():
-  return 0
-
-@pytest.fixture
-def sub_diag_offset():
-  return -1
-
-@pytest.fixture
-def super_diag_offset():
-  return 1
-
-@pytest.fixture
-def diag_elements():
-  return [1, 2, 3, 4, 5]
-
-@pytest.fixture
-def sub_diag_elements():
-  return [1, 2, 3, 4]
-
-@pytest.fixture
-def super_diag_elements():
-  return [1, 2, 3, 4]
-
-@pytest.fixture
-def true_mean(diag_elements, sub_diag_elements, super_diag_elements):
-  matrix = [diag_elements, sub_diag_elements, super_diag_elements]
-  all_elements = [val for diag in matrix for val in diag]
-  return np.mean(all_elements)
-
-# ------ Dia matrices 
-
-def main_diagonal():
-  pass
-
-def main_and_super_diagonal():
-  pass
-
-def main_and_sub_diagonal():
-  pass
-
-def sub_diagonal():
-  pass
-
-def super_diagonal():
-  pass
-
-def super_sub_diagonal():
-  pass
-
-# ------ CSR matrices 
-
-# TODO: treat special cases like operating on differences/fractions/sums of matrices to check for numerical noise
-
-def test_csr_matrix():
-  N = 3
-
-  data = np.array([4.0, -4.0, 1.0], dtype=complex)
-  col_index = np.array([0, 2, 2], dtype=np.int32)
-  row_index = np.array([0, 1, 2, 3], dtype=np.int32)
-
-  matrix = CSR((data, col_index, row_index), shape=(N, N))
-
-  expected = 1.0/3.0
-  result = mean_csr(matrix)
-  assert np.isclose(result, expected, qt.settings.core['atol'])
-
-
 class TestMean(testing.UnaryOpMixin):
   
   def op_numpy(self, matrix):
-    return np.mean(matrix)
+    nnz = np.count_nonzero(matrix)
+
+    if nnz == 0:
+      return 0.0
+
+    mean_val = matrix.sum() / nnz
+    return mean_val
 
   specialisations = [
       pytest.param(mean_csr, CSR, numbers.Complex),
       pytest.param(mean_dia, Dia, numbers.Complex),
       pytest.param(mean_dense, Dense, numbers.Complex),
   ]
-
-
-
-# Dia matrices
-
-# def test_dia_matrix_main_diag():
-#   N = 3
-# 
-#   diag_data = np.array([1, 1, 2], dtype=complex)
-#   offsets = np.array([0], dtype=np.int32)
-# 
-#   matrix = Dia((diag_data, offsets), shape=(N, N))
-# 
-#   expected = 4/3
-#   result = mean_dia(matrix)
-# 
-#   assert np.isclose(result, expected, qt.settings.core['atol'])
-
-
-# ------ Dense matrices 
-
-def dense_matrix():
-  pass
-
-
-