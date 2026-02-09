9726619760be6535122a0a9c4c10ed586b0ba9f9
Your Name <veronika.rovnik@posteo.de>
2026-02-09 11:27:43 +0900

    Format to PEP8

--- qutip/tests/core/data/test_mean.py
+++ qutip/tests/core/data/test_mean.py
@@ -7,19 +7,19 @@ from qutip.core.data.mean import mean_csr, mean_dia, mean_dense
 from qutip.core.data import CSR, Dia, Dense
 from . import test_mathematics as testing
 
+
 class TestMean(testing.UnaryOpMixin):
-  
-  def op_numpy(self, matrix):
-    nnz = np.count_nonzero(matrix)
+    def op_numpy(self, matrix):
+        nnz = np.count_nonzero(matrix)
 
-    if nnz == 0:
-      return 0.0
+        if nnz == 0:
+            return 0.0
 
-    mean_val = matrix.sum() / nnz
-    return mean_val
+        mean_val = matrix.sum() / nnz
+        return mean_val
 
-  specialisations = [
-      pytest.param(mean_csr, CSR, numbers.Complex),
-      pytest.param(mean_dia, Dia, numbers.Complex),
-      pytest.param(mean_dense, Dense, numbers.Complex),
-  ]
+    specialisations = [
+        pytest.param(mean_csr, CSR, numbers.Complex),
+        pytest.param(mean_dia, Dia, numbers.Complex),
+        pytest.param(mean_dense, Dense, numbers.Complex),
+    ]
