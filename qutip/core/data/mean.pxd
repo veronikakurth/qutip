8e18df989cd86064ec156ae3dac810e62b87945e
Veronika Kurth <veronika.kurth@posteo.de>
2026-02-20 15:55:59 +0900

    Remove calls to numpy to be able to run functions without GIL; TODO: how to replace calling python attribute settings.core['atol']?

--- qutip/core/data/mean.pxd
+++ qutip/core/data/mean.pxd
@@ -3,12 +3,12 @@
 
 from qutip.core.data cimport CSR, Dense, Dia
 
-cpdef double complex mean_csr(CSR matrix) noexcept
-cpdef double complex mean_dia(Dia matrix) noexcept
-cpdef double complex mean_dense(Dense matrix) noexcept
+cpdef double complex mean_csr(CSR matrix) noexcept nogil
+cpdef double complex mean_dia(Dia matrix) noexcept nogil
+cpdef double complex mean_dense(Dense matrix) noexcept nogil
 
-cpdef double mean_abs_csr(CSR matrix) noexcept
-cpdef double mean_abs_dia(Dia matrix) noexcept
-cpdef double mean_abs_dense(Dense matrix) noexcept
+cpdef double mean_abs_csr(CSR matrix) noexcept nogil
+cpdef double mean_abs_dia(Dia matrix) noexcept nogil
+cpdef double mean_abs_dense(Dense matrix) noexcept nogil
 
 
--- qutip/core/data/mean.pyx
+++ qutip/core/data/mean.pyx
@@ -1,16 +1,18 @@
 # cython: language_level=3
 # cython: boundscheck=False, wraparound=False, initializedcheck=False
-import numpy as np
 from qutip import settings
 from qutip.core.data cimport CSR, Dia, Dense
 
 cdef extern from "<complex>" namespace "std":
     double abs(double complex z) nogil
 
+cdef inline bint isclose(double complex z, double atol) nogil:
+    return abs(z.real) <= atol and abs(z.imag) <= atol
+
 # This module is meant to be accessed by dot-access (e.g. mean.mean_csr).
 __all__ = []
 
-cpdef double complex mean_csr(CSR matrix) noexcept:
+cpdef double complex mean_csr(CSR matrix) noexcept nogil:
     cdef size_t nnz, ptr, nnz_corrected = 0
     cdef double complex mean = 0
 
@@ -20,7 +22,7 @@ cpdef double complex mean_csr(CSR matrix) noexcept:
         return 0.0
 
     for ptr in range(nnz):
-        if np.isclose(matrix.data[ptr], 0.0, atol=settings.core['atol']):
+        if isclose(matrix.data[ptr], atol=settings.core['atol']):
             continue
         mean += matrix.data[ptr]
         nnz_corrected += 1
@@ -36,7 +38,7 @@ cdef inline int int_max(int a, int b) noexcept nogil:
 cdef inline int int_min(int a, int b) noexcept nogil:
     return a if a < b else b
 
-cpdef double complex mean_dia(Dia matrix) noexcept:
+cpdef double complex mean_dia(Dia matrix) noexcept nogil:
     cdef int offset, diag, start, end, col=1
     cdef double complex mean = 0
     cdef size_t nnz = 0
@@ -50,7 +52,7 @@ cpdef double complex mean_dia(Dia matrix) noexcept:
 
         for col in range(start, end):
             cur_el = matrix.data[diag * matrix.shape[1] + col]
-            if np.isclose(cur_el, 0.0, atol=settings.core['atol']):
+            if isclose(cur_el, atol=settings.core['atol']):
                 continue
             mean += cur_el
             nnz += 1
@@ -59,14 +61,14 @@ cpdef double complex mean_dia(Dia matrix) noexcept:
     return mean / nnz
 
 
-cpdef double complex mean_dense(Dense matrix) noexcept:
+cpdef double complex mean_dense(Dense matrix) noexcept nogil:
     cdef size_t ptr, nnz = 0
     cdef double complex mean = 0, cur
 
     for ptr in range(matrix.shape[0] * matrix.shape[1]):
         cur = matrix.data[ptr]
 
-        if np.isclose(cur, 0.0, atol=settings.core['atol']):
+        if isclose(cur, atol=settings.core['atol']):
             continue
         mean += cur
         nnz += 1
@@ -75,7 +77,7 @@ cpdef double complex mean_dense(Dense matrix) noexcept:
         return 0.0
     return mean / nnz
 
-cpdef double mean_abs_csr(CSR matrix) noexcept:
+cpdef double mean_abs_csr(CSR matrix) noexcept nogil:
     cdef size_t nnz, ptr, nnz_corrected = 0
     cdef double mean = 0
 
@@ -84,8 +86,8 @@ cpdef double mean_abs_csr(CSR matrix) noexcept:
     if nnz == 0:
         return 0.0
 
-    for ptr in range(nnz): # TODO: given the automatic tidyup, is it possible that we get very small values in CSR due to precision errors? If not, would it be correct to fill out very small values - what if they are desirable?
-        if np.isclose(matrix.data[ptr], 0.0, atol=settings.core['atol']):
+    for ptr in range(nnz):
+        if isclose(matrix.data[ptr], atol=settings.core['atol']):
             continue
         else:
             mean += abs(matrix.data[ptr])
@@ -96,7 +98,7 @@ cpdef double mean_abs_csr(CSR matrix) noexcept:
 
     return mean / nnz_corrected
 
-cpdef double mean_abs_dia(Dia matrix) noexcept:
+cpdef double mean_abs_dia(Dia matrix) noexcept nogil:
     cdef int offset, diag, start, end, col=1
     cdef double mean_abs = 0
     cdef size_t nnz = 0
@@ -111,7 +113,7 @@ cpdef double mean_abs_dia(Dia matrix) noexcept:
 
         for col in range(start, end):
             cur_el = matrix.data[diag * matrix.shape[1] + col]
-            if np.isclose(cur_el, 0.0, atol=settings.core['atol']):
+            if isclose(cur_el, atol=settings.core['atol']):
                 continue
             mean_abs += abs(matrix.data[diag * matrix.shape[1] + col])
             nnz += 1
@@ -119,12 +121,12 @@ cpdef double mean_abs_dia(Dia matrix) noexcept:
         return 0.0
     return mean_abs / nnz
 
-cpdef double mean_abs_dense(Dense matrix) noexcept:
+cpdef double mean_abs_dense(Dense matrix) noexcept nogil:
     cdef size_t ptr, nnz = 0
     cdef double mean_abs = 0, cur
     for ptr in range(matrix.shape[0] * matrix.shape[1]):
         cur = abs(matrix.data[ptr])
-        if np.isclose(cur, 0.0, atol=settings.core['atol']):
+        if isclose(cur, atol=settings.core['atol']):
             continue
         mean_abs += cur
         nnz += 1
